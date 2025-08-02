0617fdc7f6f31dd1e79f31ea78e49f3dc910d82e
Claudio Nascimento <36788885+ClaudioFN@users.noreply.github.com>
2025-08-02 12:26:31 -0300

    Incluindo codificacao de API de Catalogo de Produtos usado para treinamento no curso

---
+++ curso-apis-csharp/APICatalogo/APICatalogo/Migrations/20250731224828_PopulaCategorias.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into Categorias(Nome, ImagemUrl) Values('Bebidas', 'bebidas.jgp')");
+            migrationBuilder.Sql("insert into Categorias(Nome, ImagemUrl) Values('Lanches', 'lanches.jgp')");
+            migrationBuilder.Sql("insert into Categorias(Nome, ImagemUrl) Values('Sobremesas', 'sobremesas.jgp')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Categorias");
+        }
+    }
+}
